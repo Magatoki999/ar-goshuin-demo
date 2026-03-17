5f837fcd369c728eeccf349d1179c8ecf6daf0ef
MagatokiOhma <Kogusan@gmail.com>
2026-03-17 16:38:59 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,16 +1,20 @@
 // sw.js
-const CACHE_NAME = 'goshuin-ar-v1';
+const CACHE_NAME = 'goshuin-ar-v2';
 
 // キャッシュしておきたいローカルファイルの一覧
 const urlsToCache = [
   './',
   './index.html',
+  './shrines.json',
   './targets.mind',
   './ume_petal.png',
   './kamon.png',
   './ink_aura.png',
   './oritsuru_merrygoround.glb',
+  // './awataguchi_model.glb',
+  //'./awataguchi_audio.mp3',
   './tenmangu_ambient.mp3'
+
 ];
 
 // インストール時にファイルをキャッシュに保存
